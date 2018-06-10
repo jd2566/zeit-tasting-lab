156e6c577f21b3bd20b0f3a03b47fee402c514d7
bolinli <bolinli@iii.org.tw>
2018-06-10 14:08:50 +0800

    controller init completed.

--- app/controllers/root_categories_controller.rb
+++ app/controllers/root_categories_controller.rb
@@ -1,2 +1,51 @@
 class RootCategoriesController < ApplicationController
+  before_action :set_root_category, only: [:show, :update, :destroy]
+
+  # GET /root_categories
+  def index
+    @root_categories = RootCategory.all
+
+    render json: @root_categories
+  end
+
+  # GET /root_categories/1
+  def show
+    render json: @root_category
+  end
+
+  # POST /root_categories
+  def create
+    @root_category = RootCategory.new(root_category_params)
+
+    if @root_category.save
+      render json: @root_category, status: :created, location: @root_category
+    else
+      render json: @root_category.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /root_categories/1
+  def update
+    if @root_category.update(root_category_params)
+      render json: @root_category
+    else
+      render json: @root_category.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /root_categories/1
+  def destroy
+    @root_category.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_root_category
+      @root_category = RootCategory.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def root_category_params
+      params.fetch(:root_category, {})
+    end
 end
--- test/controllers/menus_controller_test.rb
+++ test/controllers/menus_controller_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class MenusControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @menu = menus(:one)
+  end
+
+  test "should get index" do
+    get menus_url, as: :json
+    assert_response :success
+  end
+
+  test "should create menu" do
+    assert_difference('Menu.count') do
+      post menus_url, params: { menu: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show menu" do
+    get menu_url(@menu), as: :json
+    assert_response :success
+  end
+
+  test "should update menu" do
+    patch menu_url(@menu), params: { menu: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy menu" do
+    assert_difference('Menu.count', -1) do
+      delete menu_url(@menu), as: :json
+    end
+
+    assert_response 204
+  end
 end
