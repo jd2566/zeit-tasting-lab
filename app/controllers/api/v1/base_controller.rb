0ee113422b4d53a58769ef2befcd0932683cbf99
bolinli <bolinli@iii.org.tw>
2018-06-15 09:54:39 +0800

    remove knock gem codes.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::BaseController < ApplicationController
-  include Knock::Authenticable
   protect_from_forgery with: :null_session
   before_action :destroy_session
-  before_action :authenticate_user
 
   def destroy_session
     request.session_options[:skip] = true
