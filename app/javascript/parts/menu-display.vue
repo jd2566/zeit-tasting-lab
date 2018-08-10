ca2099b66a4ad34f7e5a5f835b12c235b6a8ebd3
bolinli <bolinli@iii.org.tw>
2018-08-10 16:44:29 +0800

    mobile display update.

--- app/javascript/parts/menu-display.vue
+++ app/javascript/parts/menu-display.vue
@@ -1,12 +1,12 @@
 <template>
-  <el-container>
-    <el-header style="height:10vh;">
-      <h2 class="ui horizontal divider header" style="margin:20px;">ZEIT x LINCK</h2>
+  <el-container style="">
+    <el-header style="height:5vh;">
+      <h1 class="ui horizontal divider header" style="margin:20px; font-size:3rem">ZEIT x LINCK</h1>
     </el-header>
 
     <el-main style="height:88vh;">
       <div class="ui stackable two column centered grid">
-        <div class="column" v-for="section in sections" :key="section.id">
+        <div class="sixteen wide mobile eight wide computer column" v-for="section in sections" :key="section.id">
           <div class="ui piled segment">
             <h1 class="ui horizontal divider header">
               {{ section.name }}
@@ -15,11 +15,11 @@
             <div class="ui vertically divided grid">
               <div class="row" v-for="item in section.items" :key="item.id">
                 <div class="five wide column">
-                  <img v-if="item.images.length > 0" class="ui fluid centered rounded small image" :src="item.images[0].url">
-                  <img v-else class="ui fluid centered rounded small image" src="/images/zeit.jpg">
+                  <img v-if="item.images.length > 0" class="ui fluid centered rounded image" :src="item.images[0].url">
+                  <img v-else class="ui fluid centered rounded image" src="/images/zeit.jpg">
                 </div>
                 <div class="eleven wide column">
-                  <h2 class="ui dividing header">
+                  <h2 class="ui dividing header" style="font-size:3rem">
                     <span v-if="locale == 'tw'">{{ item.name }}</span>
                     <span v-if="locale == 'us'">{{ item.eng }}</span>
                     <span v-if="locale == 'jp'">{{ item.jpn }}</span>
@@ -40,13 +40,13 @@
                             <div class="header">{{ match.name }}</div>
                             <div class="item">
                               <div v-for="i in match.items" :key="i.id" class="list">
-                                <a class="item">
-                                  <img v-if="i.image != undefined" class="ui tiny image" :src="i.image.url">
-                                  <img v-else class="ui tiny image" src="/images/zeit.jpg">
-                                  <div class="content">
+                                <div class="item">
+                                  <img v-if="i.image != undefined" class="ui small image middle aligned" :src="i.image.url">
+                                  <img v-else class="ui small image middle aligned" src="/images/zeit.jpg">
+                                  <div class="middle aligned content">
                                     <div class="header">{{ i.name }}</div>
                                   </div>
-                                </a>
+                                </div>
                               </div>
                             </div>
                           </div>
@@ -83,7 +83,6 @@
     mounted() {
       this.loading = true;
       this.$http.get('/menu/' + this.menuId + '/' + this.lang + '/data/').then(response => {
-        debugger
         this.sections = response.body;
         this.loading = false;
       }, response => {
@@ -100,5 +99,7 @@
 </script>
 
 <style scoped>
-
+.header {
+  font-size: 2.5rem;
+}
 </style>
